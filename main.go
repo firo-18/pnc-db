8235a14a0a57e772c5f2a9ca352304153af2740c
Firo <phyllo.k@icloud.com>
2022-11-20 00:29:30 -0800

    move upcoming Dolls to a seperate folder, and write a func to move them into production if requirements are met

--- main.go
+++ main.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/firo-18/pnc/info"
 )
 
 func main() {
-	// meta := info.NewMetaData()
-	// meta.WriteYAML()
-
-	// checkDollSkills("Abigail")
+	QualityControl()
+	meta := info.NewMetaData()
+	meta.WriteYAML()
 }
 
-func checkDollSkills(name string) {
-	d := info.NewDoll()
-	d.ReadYAML(name)
-	log.Println(d.Skills.Passive.Desc)
-	log.Println(d.Skills.Auto.Desc)
-	log.Println(d.Skills.Ultimate.Desc)
+func QualityControl() {
+	path := "data/dolls/upcoming/"
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatalln("read-dir:", err)
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".yaml") {
+			doll := info.DollProfile{}
+			doll.ReadYAML(file.Name(), path)
+			if doll.Verify() {
+				err := os.Rename(path+file.Name(), "data/dolls/"+file.Name())
+				if err != nil {
+					log.Fatalln("rename:", err)
+				}
+				err = os.Rename("asset/dolls/icons/upcoming/"+doll.Name+".png", "asset/dolls/icons/"+doll.Name+".png")
+				if err != nil {
+					log.Fatalln("rename:", err)
+				}
+				log.Printf("Successfully move file and assets of Doll `%v' to production.", doll.Name)
+			}
+		}
+	}
 }
